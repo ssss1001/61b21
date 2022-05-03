6712c303b7dc77163658d0d073398d843a4e3e08
ssss1001 <1574261954@qq.com>
2022-05-03 11:13:20 +0800

    lab4的第一次提交

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -1,16 +1,20 @@
-/** Class that prints the Collatz sequence starting from a given number.
- *  @author YOUR NAME HERE
+/**
+ * Class that prints the Collatz sequence starting from a given number.
+ *
+ * @author YOUR NAME HERE
  */
 public class Collatz {
 
-    /** Buggy implementation of nextNumber! */
+    /**
+     * Buggy implementation of nextNumber!
+     */
     public static int nextNumber(int n) {
 
-        if (n % 2 != 0){
-            return (3 * n + 1);
-        }else {
-            return n / 2;
-        }
+//        if (n % 2 != 0){
+//            return (3 * n + 1);
+//        }else {
+//            return n / 2;
+//        }
 
 //        if (n  == 128) {
 //            return 1;
@@ -19,6 +23,16 @@ public class Collatz {
 //        } else {
 //            return n * 2;
 //        }
+        /** Buggy implementation of nextNumber! */
+        /*错误实现方法*/
+        if (n == 128) {
+            return 1;
+        } else if (n == 5) {
+            return 3 * n + 1;
+        } else {
+            return n * 2;
+        }
+
     }
 
     public static void main(String[] args) {
