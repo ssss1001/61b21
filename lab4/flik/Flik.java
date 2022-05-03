c1cce7d0e7e0a0442e6ade4fab7a73e23c2b4d8c
ssss1001 <1574261954@qq.com>
2022-05-03 17:33:41 +0800

    lab4的调试提交

--- lab4/flik/Flik.java
+++ lab4/flik/Flik.java
@@ -1,5 +1,8 @@
 package flik;
 
+import afu.org.checkerframework.checker.igj.qual.I;
+import org.junit.Test;
+
 /** An Integer tester created by Flik Enterprises.
  * @author Josh Hug
  * */
@@ -8,6 +11,19 @@ public class Flik {
      *  @param b Value 2
      *  @return Whether a and b are the same */
     public static boolean isSameNumber(Integer a, Integer b) {
-        return a == b;
+        return a.intValue() == b.intValue();
+    }
+
+
+    @Test
+    public void test(){
+        Integer a = new Integer(1);
+        Integer b = new Integer(2);
+        System.out.println(isSameNumber(a, b));
+
+        a = 2;
+        System.out.println(a);
+        System.out.println(isSameNumber(a, b));
     }
 }
+
